434d880ffca89a0940cc8e4a80307d53c7725106
Andrew Dabski <jedrus1133@gmail.com>
2017-04-06 03:13:19 +0200

    reset unnesesary changes

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from pygame import Color
 game_title = "Flying strawberry"
 screen_size = (800, 500)
 lock_fps = 60
-background = Color("Black")
+background = Color("black")
 pipes_color = Color("Orange")
 font_color = Color("White")
 
-gravity_vector = (0, 0.18)
+gravity_vector = (0, 0.17)
 jump_vector = (0, -8)
 init_pos = (70, 300)
 player_size = (25,25)
