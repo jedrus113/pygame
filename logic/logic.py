4a7de57d4881d8cd78f7ca7a7477ba0ac74a9d5e
Andrew Dabski <jedrus1133@gmail.com>
2017-04-06 04:26:26 +0200

    loop by objects in logic

--- logic/logic.py
+++ logic/logic.py
@@ -25,8 +25,9 @@ class GameLogic:
             if pygame.K_SPACE in self.keys:
                 self.strawberry.addVector(config.World.jump_vector)
             self.strawberry.addVector(config.World.gravity_vector)
-            self.strawberry.on_loop()
-            self.pipe.on_loop()
+
+            for thing in config.World.objects:
+                thing.on_loop()
 
             # end level condition
             if self.strawberry.getPos()[1] + config.Player.size[1] > config.Window.size[1] or self.strawberry.getPos()[1] < 0 or ((self.strawberry.getPos()[0]+config.Player.size[0]-10 > self.pipe.getPos()[0] and self.strawberry.getPos()[0]-40 < self.pipe.getPos()[0]) and (self.strawberry.getPos()[1]+10 < self.pipe.height or self.strawberry.getPos()[1]+config.Player.size[1]-10 > self.pipe.height + self.pipe.size)):
@@ -34,5 +35,5 @@ class GameLogic:
                 self.on_init()
             elif self.pipe.getPos()[0] < 0:
                 config.World.score += 1
-                self.on_init()
+                self.pipe = Pipe()
         self.keys = []
